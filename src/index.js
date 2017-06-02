1c003f71f3597a47ede509f7106d90e148f88828
Mario Viens <marviens@gmail.com>
2017-06-01 19:18:01 -0700

    generic commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,36 @@
-require('dotenv').config();
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar';
+require('dotenv').config()
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+
+const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY || 'AIzaSyC9Zrk1hDlPQgUqEFc32NvCxmPRyMCaXyU'
 
 
-const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 
 // Create a new component. This component should produce
 // some HTML
-const App = () => {
-  return  (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: [] }
+
+    YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos })
+    })
+  }
+  render () {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
 }
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'))
