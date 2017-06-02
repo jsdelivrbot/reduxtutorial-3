cd6af90df3c227187e99d3014e6419bd2ea90bc9
Mario Viens <marviens@gmail.com>
2017-06-01 21:04:26 -0700

    added boilerplate for videos

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-require('dotenv').config()
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import YTSearch from 'youtube-api-search'
-import SearchBar from './components/search_bar'
-import VideoList from './components/video_list'
+require('dotenv').config();
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY || 'AIzaSyC9Zrk1hDlPQgUqEFc32NvCxmPRyMCaXyU'
+const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY || 'AIzaSyC9Zrk1hDlPQgUqEFc32NvCxmPRyMCaXyU';
 
 
 
@@ -13,24 +13,24 @@ const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY || 'AIzaSyC9Zrk1hDlPQgUqEFc3
 // some HTML
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.state = { videos: [] }
+    this.state = { videos: [] };
 
     YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos })
-    })
-  }
+      this.setState({ videos });
+    });
+  };
   render () {
     return (
       <div>
         <SearchBar />
         <VideoList videos={this.state.videos} />
       </div>
-    )
+    );
   }
 }
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector('.container'))
+ReactDOM.render(<App />, document.querySelector('.container'));
