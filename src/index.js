478cc89f8d351e05a8c7d152fc6340e8eae68a03
Mario Viens <marviens@gmail.com>
2017-12-07 13:26:36 -0800

    Search field fires on button press - 2.31

--- src/index.js
+++ src/index.js
@@ -21,17 +21,22 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, (videos) => {
+    this.videoSearch('surfboards');
+  };
+
+  videoSearch(term) {
+    YTSearch({key: YOUTUBE_API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
       });
     });
-  };
+  }
+
   render () {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term )} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
